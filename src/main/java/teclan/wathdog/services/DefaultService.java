603e9d533cc7960743f40c33c2993f59c67162f6
谭炳健 <tbj621@163.com>
2020-07-28 16:06:33 +0800

    整理代码

--- src/main/java/teclan/wathdog/services/DefaultService.java
+++ src/main/java/teclan/wathdog/services/DefaultService.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import teclan.exec.Executor;
 import teclan.wathdog.config.CommonConfig;
+import teclan.wathdog.utils.FileUtils;
 import teclan.wathdog.utils.ResultUtils;
 
 import java.io.File;
@@ -20,11 +21,14 @@ public class DefaultService {
         String batShell = APP_ROOT + File.separator + APP_NAME + File.separator + shell;
         try {
             LOGGER.info("即将执行脚本:{}", batShell);
+            if(!FileUtils.exists(new File(shell))){
+                throw  new Exception("脚本文件不存在");
+            }
             Executor.exec(batShell);
             return ResultUtils.getResult(200, "脚本执行成功,"+batShell);
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
-            return ResultUtils.getResult(200, "脚本执行失败,"+batShell, e.getMessage());
+            return ResultUtils.getResult(500, "脚本执行失败,"+batShell, e.getMessage());
         }
     }
 
